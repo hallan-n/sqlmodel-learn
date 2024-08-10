e3159e682c7839246003c988d6bcb182e2f2bb1a
hallan-n <hallanneves01@gmail.com>
2024-08-09 23:36:27 -0300

    CRUD finish

--- main.py
+++ main.py
@@ -1,34 +1,28 @@
-from sqlmodel import SQLModel, Field, create_engine, Session, select
+from sqlmodel import Relationship, SQLModel, Field, create_engine, Session
 from typing import Optional
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     name: str
     email: str
+    profile: Optional["Profile"] = Relationship(back_populates="user")
+
 
 class Profile(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     profession: str
     description: str
-    user_id: int = Field(foreign_key='user.id')
+    user_id: int = Field(foreign_key="user.id")
+    user: Optional[User] = Relationship(back_populates="profile")
 
 
-engine = create_engine('sqlite:///database.db')
+engine = create_engine("sqlite:///database.db")
 SQLModel.metadata.create_all(engine)
-
-class Connetion:
-    def __init__(self) -> None:
-        self.session = Session(engine)
-
-    def __enter__(self):
-        return self.session
-
-    def __exit__(self, type, value, traceback):
-        self.session.close()
 
 
 def create_user(user: User, profile: Profile = None):
-    with Connetion() as conn:
+    with Session(engine) as conn:
         conn.add(user)
         conn.commit()
         conn.refresh(user)
@@ -38,19 +32,27 @@ def create_user(user: User, profile: Profile = None):
             conn.add(profile)
             conn.commit()
 
-def get_all_users(profiles: bool = None):
-    with Connetion() as conn:
-        if profiles:
-            query = select(User, Profile).join(Profile).where(User.id == Profile.user_id)
-            users = conn.exec(query).all()
-            return users
-        
-        query = select(User)
-        users = conn.exec(query).all()
-        return users
 
-user = User(name="Hállan", email="hallan@neves.com")
-profile = Profile(profession='Dev', description='Coda fofo')
+def update_user(user: User):
+    with Session(engine) as conn:
+        existing_user = conn.get(User, user.id)
+        if existing_user:
+            merged_user = conn.merge(user)
+            conn.commit()
+            conn.refresh(merged_user)
+            return merged_user
 
-# create_user(user, profile)
-# print(get_all_users(profiles=False))
+
+def get_user(id: int):
+    with Session(engine) as conn:
+        user = conn.get(User, id)
+        return user, user.profile
+
+
+def delete_user(id: int):
+    with Session(engine) as conn:
+        user = conn.get(User, id)
+        if user:
+            conn.delete(user.profile)
+            conn.delete(user)
+            conn.commit()
